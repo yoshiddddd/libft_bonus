97e786911db121b6b64810af552c9fb54c8527b0
Kazushi Yoshida <kyoshida@c1r16s4.42tokyo.jp>
2023-07-04 21:09:56 +0900

    sa

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 20:33:09 by kyoshida          #+#    #+#             */
+/*   Updated: 2023/07/04 20:36:53 by kyoshida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*tmp;
+
+	tmp = (t_list *)malloc(sizeof(tmp));
+	if (!tmp)
+		return (NULL);
+	tmp->content = content;
+	tmp->next = NULL;
+	return (tmp);
+}
